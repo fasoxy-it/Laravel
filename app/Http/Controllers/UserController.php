c9254cacb878400ae41400a80ce69560dd823326
fasoxy-it <fasoxy@gmail.com>
2021-03-11 15:33:06 +0100

    Raw SQL Query
    Insert Data into MySQL Database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function index() {
+        DB::insert('insert into users (name, email, password) values (?, ?, ?)', ['fasoxy', 'fasoxy@gmail.com', 'password']);
         return view('home');
     }
 }
