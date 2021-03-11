2b1003607ea9b5cba6450214bdd2579e6a671ff9
fasoxy-it <fasoxy@gmail.com>
2021-03-11 15:56:16 +0100

    Raw SQL Query
    Update Data into MySQL Database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,9 @@ class UserController extends Controller
 {
     public function index() {
         //DB::insert('insert into users (name, email, password) values (?, ?, ?)', ['fasoxy', 'fasoxy@gmail.com', 'password']);
+        //$users = DB::select('select * from users');
+        //return $users;
+        DB::update('update users set name = ? where id = 1', ['Mattia']);
         $users = DB::select('select * from users');
         return $users;
         return view('home');
