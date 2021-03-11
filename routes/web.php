39b20d946957c6e443ea073eff3df877138bbe9b
fasoxy-it <fasoxy@gmail.com>
2021-03-11 12:50:20 +0100

    Routing
    Change Route and Create Controller by using Command Line (php artisan make:controller UserController)

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/user', function () {
-    return view('home');
-});
+Route::get('/user', 'UserController@index');
+
+
