2b477183b81bd18c03fe3ca14619ff54f565da6a
fasoxy-it <fasoxy@gmail.com>
2021-03-12 10:05:18 +0100

    Laravel UI
    Install Laravel UI by using Command Line (composer require laravel/ui)

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,11 +41,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function setPasswordAttribute($password) {
-        $this->attributes['password'] = bcrypt($password);
-    }
+    //public function setPasswordAttribute($password) {
+    //    $this->attributes['password'] = bcrypt($password);
+    //}
 
-    public function getNameAttribute($name) {
-        return ucfirst($name);
-    }
+    //public function getNameAttribute($name) {
+    //    return ucfirst($name);
+    //}
 }
