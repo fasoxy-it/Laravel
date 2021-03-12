09bb18bf69e35062588b5dbd3664f6d973e93eb7
fasoxy-it <fasoxy@gmail.com>
2021-03-12 12:24:44 +0100

    Upload Image
    Verify Upload Image

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 Route::get('/user', 'UserController@index');
 
 Route::post('/upload', function(Request $request){
-    dd($request->image);
+    dd($request->hasFile('image'));
 });
 
 
