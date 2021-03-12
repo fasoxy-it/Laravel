1449085f386df0733cc34fb5610767dc16b19078
fasoxy-it <fasoxy@gmail.com>
2021-03-12 12:16:29 +0100

    Upload Image
    Change Upload Function

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -20,8 +21,8 @@ Route::get('/', function () {
 
 Route::get('/user', 'UserController@index');
 
-Route::post('/upload', function(){
-    dd(request()->all());
+Route::post('/upload', function(Request $request){
+    dd($request->all());
 });
 
 
