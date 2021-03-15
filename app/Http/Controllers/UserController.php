d20ae0b9add1fc10f861af5c13e589c9a405a211
fasoxy-it <fasoxy@gmail.com>
2021-03-15 14:45:22 +0100

    Delete Image
    Delete Previous Storaged Image before Storage and Upload New Image

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use App\Models\User;
 
 class UserController extends Controller
@@ -53,6 +54,9 @@ class UserController extends Controller
     public function uploadAvatar(Request $request) {
         if($request->hasFile('image')) {
             $filename = $request->image->getClientOriginalName();
+            if(auth()->user()->avatar) {
+                Storage::delete('/public/images/' . auth()->user()->avatar);
+            }
             $request->image->storeAs('images', $filename, 'public');
             auth()->user()->update(['avatar' => $filename]);
         }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,9 @@
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
-                                    <img src="{{ asset('/storage/images/' . Auth::user()->avatar) }}" alt="avatar" height="10"/>
+                                    @if (Auth::user()->avatar)
+                                        <img src="{{ asset('/storage/images/' . Auth::user()->avatar) }}" alt="avatar" height="10"/>
+                                    @endif
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
