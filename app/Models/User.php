02b146d7ff65835051743e798676e3954aa82333
fasoxy-it <fasoxy@gmail.com>
2021-03-16 10:20:05 +0100

    Update Image
    Add Functions in Model that Update Image

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 
 class User extends Authenticatable
 {
@@ -41,6 +42,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function uploadAvatar($image) {
+        $filename = $image->getClientOriginalName();
+        (new self())->deleteOldImage();
+        $image->storeAs('images', $filename, 'public');
+        auth()->user()->update(['avatar' => $filename]);
+    }
+
+    protected function deleteOldImage() {
+        if($this->avatar) {
+            Storage::delete('/public/images/' . $this->avatar);
+        }
+    }
 
     //public function setPasswordAttribute($password) {
     //    $this->attributes['password'] = bcrypt($password);
