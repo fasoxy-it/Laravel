f8f4f146c8bbe50b0592ac1beb6f80057dc3f013
fasoxy-it <fasoxy@gmail.com>
2021-03-23 12:13:49 +0100

    ToDoList
    Define Relationship between ToDos and Steps

--- app/Models/ToDo.php
+++ app/Models/ToDo.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Step;
 
 class ToDo extends Model
 {
     protected $fillable = ['title', 'completed', 'description', 'user_id'];
+
+    public function steps() {
+        return $this->hasMany(Step::class);
+    }
 }
