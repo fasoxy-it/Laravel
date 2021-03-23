435930f48b8d31db1fc7e2900b07fe2f4bad4e11
fasoxy-it <fasoxy@gmail.com>
2021-03-23 10:11:29 +0100

    ToDoList
    Add ToDo Description and Fresh Migrate Tables to MySQL Database by using Command Line (php artisan migrate:fresh)

--- database/migrations/2021_03_16_144047_create_to_dos_table.php
+++ database/migrations/2021_03_16_144047_create_to_dos_table.php
@@ -16,6 +16,7 @@ class CreateToDosTable extends Migration
         Schema::create('to_dos', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->text('description');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->boolean('completed')->default(false);
