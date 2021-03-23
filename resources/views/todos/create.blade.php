97608bf370ea499cf283670e1ac6b268514d8099
fasoxy-it <fasoxy@gmail.com>
2021-03-23 10:29:58 +0100

    ToDoList
    Create ToDo Description

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -11,8 +11,15 @@
     <x-alert/>
     <form method="post" action="{{route('todo.store')}}" class="py-5">
         @csrf
-        <input type="text" name="title" class="py-2 px-2 border rounded">
-        <input type="submit" value="Create" class="p-2 border rounded">
+        <div class="py-1">
+            <input type="text" name="title" class="py-2 px-2 border rounded" placeholder="Title">
+        </div>
+        <div class="py-1">
+            <textarea name="description" class="p-2 border rounded" placeholder="Description"></textarea>
+        </div>
+        <div class="py-1">
+            <input type="submit" value="Create" class="p-2 border rounded">
+        </div>
     </form>
 
 @endsection
